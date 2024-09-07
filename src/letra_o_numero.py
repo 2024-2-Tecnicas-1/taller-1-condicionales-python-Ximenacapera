a0ee14397310321234d2111832468221d35a7c12
Ximena Capera <ximenacapera79@gmail.com>
2024-09-07 18:20:45 -0500

    Letras o Números intento 1

--- src/letra_o_numero.py
+++ src/letra_o_numero.py
@@ -1,6 +1,14 @@
 def evaluar(caracter):
     # TODO: Coloca aquí el código del ejercicio 4: Letra o número
-    return "";
+    ascci = ord(caracter)
+    if 65 <= ascci <= 90:
+        return "Es letra mayúscula"
+    elif 97 <= ascci <= 122:
+        return "Es letra minúscula"
+    elif 48 <= ascci <= 57:
+        return "Es número"
+    else:
+        return "No es letra ni número";
 
 if __name__ == '__main__':
     print("Caracter:", end='')
