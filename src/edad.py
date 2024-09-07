362f107b02ebd42dc0fb67f145bfb3196e973d93
Ximena Capera <ximenacapera79@gmail.com>
2024-09-07 16:55:04 -0500

    primer intento

--- src/edad.py
+++ src/edad.py
@@ -1,5 +1,14 @@
+from time import localtime
 def evaluar(dia, mes, anno):
     # TODO: Coloca aquí el código del ejercicio 6: Edad
+    t = localtime()
+    dia_actual = t.tm_mday
+    mes_actual = t.tm_mon
+    anno_actual = t.tm_year
+    edad = anno_actual - anno
+    if (mes_actual, dia_actual) < (mes, dia):
+        edad -= 1
+    return "Usted tiene " + str (edad) + " años"
     return "";
 
 if __name__ == '__main__':
