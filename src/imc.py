4c264c47873b5e06727d18a1caa9d3bd32b49229
Ximena Capera <ximenacapera79@gmail.com>
2024-09-07 17:55:24 -0500

    IMC primer intento

--- src/imc.py
+++ src/imc.py
@@ -1,6 +1,18 @@
 def evaluar(peso, estatura, edad):
     # TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-    return "";
+    imc = peso / (estatura ** 2)
+    if edad < 45: 
+        if imc < 22.0:
+            condicion = "bajo"
+        elif imc >= 22.0:
+             condicion = "medio"
+    else:
+        if edad >= 45:
+            if imc >= 22.0:
+                condicion = "alto"
+            elif imc < 22.0:
+                condicion = "medio"
+    return condicion;
 
 if __name__ == '__main__':
     print("Peso:", end="")
